08a3dfbb32aa7c60740231a8cdce68873ff8a262
Adrian Kurek <adrian.kurek93@gmail.com>
2021-03-01 22:05:59 +0100

    Add CartSummary component

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { Product } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
+import { Cart } from "../model/cart.model";
 
 @Component({
     selector: "store",
@@ -13,7 +14,7 @@ export class StoreComponent {
     public productsPerPage = 4;
     public selectedPage = 1;
 
-    constructor(private repository: ProductRepository) { }
+    constructor(private repository: ProductRepository, private cart:Cart) {}
 
     get products(): Product[] {
         let pageIndex = (this.selectedPage - 1) * this.productsPerPage
@@ -51,4 +52,7 @@ export class StoreComponent {
             .map((i) => i + 1);
     } */
 
+    addProductToCart(product: Product) {
+        this.cart.addLine(product);
+    }
 }
