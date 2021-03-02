68d399ccfe98e6df5a25cc7374338cd634ba92c0
Adrian Kurek <adrian.kurek93@gmail.com>
2021-03-02 19:10:41 +0100

    Add Checkout component

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -6,11 +6,12 @@ import { StoreComponent } from './store.component';
 import { CounterDirective } from './counter.directive';
 import { CartSummaryComponent } from "./cartSummary.component";
 import { CartDetailComponent } from "./cartDetail.component";
+import { CheckoutComponent } from "./checkout.component";
 
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent, CounterDirective, CartSummaryComponent, CartDetailComponent],
-    exports: [StoreComponent, CartDetailComponent]
+    declarations: [StoreComponent, CounterDirective, CartSummaryComponent, CartDetailComponent, CheckoutComponent],
+    exports: [StoreComponent, CartDetailComponent, CheckoutComponent]
 })
 
 export class StoreModule { }
