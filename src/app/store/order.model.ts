9003f9d8b2e86861a8e24b159ad911fefe02ee18
Adrian Kurek <adrian.kurek93@gmail.com>
2021-03-03 20:27:58 +0100

    Add Order model

---
+++ src/app/store/order.model.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { Cart } from "../model/cart.model";
+
+@Injectable()
+export class Order {
+    public id: number;
+    public name: string;
+    public address: string;
+    public city: string;
+    public state: string;
+    public zip: string;
+    public country: string;
+    public shipped: boolean = false;
+
+    constructor(public cart: Cart) {}
+
+    clear() {
+        this.id = this.name =
+        this.address = this.city =
+        this.state = this.zip =
+        this.country = null;
+        this.shipped = false;
+        this.cart.clear();
+    }
+}
