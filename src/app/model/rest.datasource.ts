0c78657db1ff85e005cc77d3469dfefd4b3b2385
Adrian Kurek <adrian.kurek93@gmail.com>
2021-03-15 22:45:43 +0100

    Add some CRUD methods for Product and Order repos

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -30,12 +30,40 @@ export class RestDataSource {
         })
     }
 
+    // Product.
+
     getProducts(): Observable<Product[]> {
         return this.sendRequest(RequestMethod.Get, "products") as Observable<Product[]>;
     }
 
+    saveProduct(product: Product): Observable<Product> {
+        return this.sendRequest(RequestMethod.Post, "products", product, true) as Observable<Product>;
+    }
+
+    updateProduct(product): Observable<Product> {
+        return this.sendRequest(RequestMethod.Put, `products/${product.id}`, product, true) as Observable<Product>;
+    }
+
+    deleteProduct(id: number): Observable<Product> {
+        return this.sendRequest(RequestMethod.Delete, `products/${id}`, null, true) as Observable<Product>;
+    }
+
+    // Order.
+
+    getOrders(): Observable<Order[]> {
+        return this.sendRequest(RequestMethod.Get, "orders") as Observable<Order[]>;
+    }
+
     saveOrder(order: Order): Observable<Order> {
         return this.sendRequest(RequestMethod.Post, "orders", order) as Observable<Order>;
+    }
+
+    updateOrder(order): Observable<Order> {
+        return this.sendRequest(RequestMethod.Put, `orders/${order.id}`, order, true) as Observable<Order>;
+    }
+
+    deleteOrder(id: number): Observable<Order> {
+        return this.sendRequest(RequestMethod.Delete, `orders/${id}`, null, true) as Observable<Order>;
     }
 
     private sendRequest(verb: RequestMethod, url: string, body?: Product | Order, auth: boolean = false)
